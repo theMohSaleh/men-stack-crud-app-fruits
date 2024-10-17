d5273eb261b4d6d44f2d343c1c8e2dfed3e3dabb
Mohamed Saleh <mohxbox36021@gmail.com>
2024-10-17 10:07:15 +0300

    finished app

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // modules imports
-const dotenv = require("dotenv"); // require package
+const dotenv = require("dotenv");
 dotenv.config(); // Loads the environment variables from .env file
 const express = require("express");
-const mongoose = require("mongoose"); // require package
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
 
 const app = express();
 
@@ -17,6 +19,8 @@ mongoose.connection.on("connected", () => {
 // Import the Fruit model
 const Fruit = require("./models/fruit.js");
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
@@ -33,20 +37,51 @@ app.get("/", async (req, res) => {
 app.get("/fruits", async (req, res) => {
     const allFruits = await Fruit.find();
     res.render("fruits/index.ejs", { fruits: allFruits });
-  });
+});
 
 // GET /fruits/new
 app.get("/fruits/new", (req, res) => {
     res.render("fruits/new.ejs");
 });
 
+// GET /fruits/:fruitID
+app.get("/fruits/:fruitId", async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render("fruits/show.ejs", { fruit: foundFruit });
+});
+
 // POST /fruits
 app.post("/fruits", async (req, res) => {
     if (req.body.isReadyToEat === "on") {
-      req.body.isReadyToEat = true;
+        req.body.isReadyToEat = true;
     } else {
-      req.body.isReadyToEat = false;
+        req.body.isReadyToEat = false;
     }
     await Fruit.create(req.body);
     res.redirect("/fruits"); // redirect to index fruits
-  });
+});
+
+// GET localhost:3000/fruits/:fruitId/edit
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render('fruits/edit.ejs', {
+        fruit: foundFruit
+    });
+});
+
+app.put("/fruits/:fruitId", async (req, res) => {
+    const formData = req.body;
+    if (formData.isReadyToEat === 'on') {
+        formData.isReadyToEat = true;
+    } else {
+        formData.isReadyToEat = false;
+    }
+    await Fruit.findByIdAndUpdate(req.params.fruitId, formData)
+
+    res.redirect(`/fruits/${req.params.fruitId}`)
+})
+
+app.delete("/fruits/:fruitId", async (req, res) => {
+    await Fruit.findByIdAndDelete(req.params.fruitId);
+    res.redirect("/fruits");
+});
